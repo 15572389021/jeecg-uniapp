5696c501a56224d1342cefa93811b20df726d3b9
15572389021 <z15572389021@aliyun.com>
2020-07-27 23:42:35 +0800

    修改我的卡片页面

--- pages/card/index.vue
+++ pages/card/index.vue
@@ -4,9 +4,41 @@
 			<block slot="backText">返回</block>
 			<block slot="content">编辑卡片</block>
 		</cu-custom>
-		<view class="flex flex-wrap" v-for="card in cardList" :key="card">
-			<view class="basis-xl bg-grey margin-xs padding-sm radius">xl(80%)</view>
-		</view>
+		<scroll-view>
+			<view class="cu-card article" :class="isCard?'no-card':''">
+				<view class="cu-item shadow">
+					<view class="title"><view class="text-cut">无意者 烈火焚身;以正义的烈火拔出黑暗。我有自己的正义，见证至高的烈火吧。</view></view>
+					<view class="content">
+						<image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg"
+						 mode="aspectFill"></image>
+						<view class="desc">
+							<view class="text-content"> 折磨生出苦难，苦难又会加剧折磨，凡间这无穷的循环，将有我来终结！真正的恩典因不完整而美丽，因情感而真诚，因脆弱而自由！</view>
+							<view>
+								<view class="cu-tag bg-red light sm round">正义天使</view>
+								<view class="cu-tag bg-green light sm round">史诗</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="cu-list menu-avatar" >
+				<view class="cu-item margin-sm radius margin-bottom-xsl" style="height: 230rpx;border-radius:15rpx;" v-for="card in cardList" :key="card">
+					<view class="cu-avatar round lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg);"></view>
+						<view class="content">
+							<view class="text-grey">凯尔</view>
+							<view class="text-gray text-sm flex">
+								<view class="text-cut">
+									<text class="cuIcon-infofill text-red  margin-right-xs"></text>
+									我已天理为凭，踏入这片荒芜，不再受凡人的枷锁遏制。我已天理为凭，踏入这片荒芜，不再受凡人的枷锁遏制。
+								</view> </view>
+							</view>
+						<view class="action">
+						<view class="text-grey text-xs">22:20</view>
+						<view class="cu-tag round bg-grey sm">5</view>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -28,7 +60,7 @@
 					dueDay: '',
 					remainQuota: ''
 				},
-				cardList: [1,2,3,4]
+				cardList: [1,2,3,4,5,6,7,8,9]
 			};
 		},
 		methods: {
